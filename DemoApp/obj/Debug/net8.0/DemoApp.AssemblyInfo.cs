e4b87ca054b7f09a1406f4ed9334de7677cf65bd
josejesusguzman <jose1824@live.com.mx>
2025-06-27 09:39:15 -0600

    Clase 5

--- DemoApp/obj/Debug/net8.0/DemoApp.AssemblyInfo.cs
+++ DemoApp/obj/Debug/net8.0/DemoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81b693b1691ab85760862c4acaa6660fa7dd350")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05696fe6a3b671f4b52228c4c3fd1caee35de5dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
